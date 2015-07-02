6eaafa02107745a5521ecc3cda188f61214fbdfb
Andy <xuanjian6@gmail.com>
2015-07-02 14:08:34 +0800

    Add Script Set the workflow on the inspector & write the flow info into xml file

---
+++ scripts/WriteXML.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Author:      AndySun
+ * Date:        2015-07-02
+ * Description: Operate info will be store into oper array,and this info will be write into xml file.
+ */
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class WriteXML : MonoBehaviour {
+    public string fileName = "Config.xml";
+    public OperItem[] oper;
+
+    void FindXMLFile(string name)
+    {
+        if (!File.Exists(name))
+        {
+            File.Create(name);
+        }
+    }
+}
